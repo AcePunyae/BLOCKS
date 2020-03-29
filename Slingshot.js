004713c255aa8b0a6eaaa03b42fc6f9887c949ee
AcePunyae <ace.nagpal@gmail.com>
2020-03-29 14:21:07 +0800

    Added Rubber Band

--- Slingshot.js
+++ Slingshot.js
@@ -1,10 +1,15 @@
 class SlingShot{
     constructor(bodyA, pointB){
+        this.sling1=loadImage('sprites/sling1.png')
+        this.sling2=loadImage('sprites/sling2.png')
+        this.sling3=loadImage('sprites/sling3.png')
+        
         var options = {
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
             length: 10
+            
         }
         this.pointB = pointB
         this.sling = Constraint.create(options);
@@ -19,9 +24,27 @@ class SlingShot{
         if(this.sling.bodyA){
             var pointA = this.sling.bodyA.position;
             var pointB = this.pointB;
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            strokeWeight(8);
+            stroke(84,39,15)
+            if(pointA.x<220){
+            line(pointA.x-20,pointA.y,pointB.x-10,pointB.y)
+            line(pointA.x-30,pointA.y,pointB.x+30,pointB.y)
+            //line(pointA.x, pointA.y, pointB.x, pointB.y);
+            image(this.sling3,pointA.x-35,pointA.y-10,15,30);
         }
+        else{
+            strokeWeight(6);
+            line(pointA.x+20,pointA.y,pointB.x-10,pointB.y)
+            line(pointA.x+30,pointA.y,pointB.x+30,pointB.y)
+            //line(pointA.x, pointA.y, pointB.x, pointB.y);
+            image(this.sling3,pointA.x+25,pointA.y-20,15,30);  
+        }
+        }
+    image(this.sling1,200,20);
+    image(this.sling2,170,20);
+  
+
+
     }
     
 }
